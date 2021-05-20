b2adc036655bb6370a973e8225871afa64503386
yogitakumar <helloyogita@gmail.com>
2021-05-20 18:57:35 +0100

    Test pass for throwing exception on invalid command key

--- NasaMarsRoverTest/CommandLookUpTest.cs
+++ NasaMarsRoverTest/CommandLookUpTest.cs
@@ -28,5 +28,11 @@ namespace NasaMarsRoverTest
             var command = CommandLookup.GetCommand("M");
             Assert.IsInstanceOf(typeof(MoveForwardCommand), command);
         }
+
+        [Test]
+        public void ShouldThrowInvalidOperationExceptionForInvalidKey()
+        {
+            Assert.Throws<InvalidOperationException>(() => CommandLookup.GetCommand("X"));
+        }
     }
 }
